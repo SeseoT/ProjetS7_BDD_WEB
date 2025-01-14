e48d6e0e4ff200ace5fe6395de0fb29aa104fae0
SeseoT <tangy.suto@gmail.com>
2025-01-15 00:24:23 +0100

    TEST auto deploy

--- deploy.php
+++ deploy.php
@@ -45,7 +45,7 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }
-
+$allowed = true;
 if (!$allowed) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
