0320107d4ae6c66abfae72f1d0ed7cdf33ff2589
SeseoT <tangy.suto@gmail.com>
2025-01-21 15:22:43 +0100

    Gestion dans admin de la requete 1

--- devTest/requete1.php
+++ devTest/requete1.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['username'])) {
               </tr>';
     foreach ($result as $row) {//$row['prenom']
         $html .= '<tr>
-                   <td>' . htmlspecialchars($row) . '</td>
+                   <td>' . htmlspecialchars($row[0]) . '</td>
                   </tr>';
     }
     $html .= '</table>';
