1a8e321ae1a3b4966f16938e276d0fe890112528
SeseoT <tangy.suto@gmail.com>
2025-01-21 13:10:47 +0100

    Connexion admin

--- devTest/index.php
+++ devTest/index.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
+require("connect.php"); // Inclure la configuration de connexion
 
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
+}else{//Redirection vers les pages spécialisé des clients
+    $query = "SELECT * FROM Administrateur WHERE numAdministrateur = :id_user";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':id_user', $_SESSION['id_user']);
+    $stmt->execute();
+    if($stmt->rowCount() > 0){
+        $_SESSION['role'] = "Admin";
+        header("Location: indexAdmin.php"); // Rediriger vers le tableau de bord
+        exit();
+    }
 }
 
 ?>
