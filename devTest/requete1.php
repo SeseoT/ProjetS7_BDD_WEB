ece7d0523bb417a176be00ee37188368bbf15faa
SeseoT <tangy.suto@gmail.com>
2025-01-21 16:03:50 +0100

    Gestion dans admin de la requete 1

--- devTest/requete1.php
+++ devTest/requete1.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username'])) {
     exit();
 }else{//Redirection vers les pages spécialisé des clients
     try {
-        $sql = "SELECT prenom, nom, Utilisateur.adresse, Club.nomClub, Club.departement, Club.region, dateDebut, dateFin FROM Utilisateur, Club, Concours,Competiteur,CompetiteurParticipe WHERE Utilisateur.numUtilisateur = Competiteur.numCompetiteur AND Concours.numConcours = CompetiteurParticipe.numConcours AND CompetiteurParticipe.numCompetiteur = Competiteur.numCompetiteur AND Club.numClub = Utilisateur.numClub AND YEAR(Concours.dateFin) = '2023' ";
+        $sql = "SELECT DISTINCT prenom, nom, Utilisateur.adresse, Club.nomClub, Club.departement, Club.region, dateDebut, dateFin FROM Utilisateur, Club, Concours,Competiteur,CompetiteurParticipe WHERE Utilisateur.numUtilisateur = Competiteur.numCompetiteur AND Concours.numConcours = CompetiteurParticipe.numConcours AND CompetiteurParticipe.numCompetiteur = Competiteur.numCompetiteur AND Club.numClub = Utilisateur.numClub AND YEAR(Concours.dateFin) = '2023' ";
         $stmt = $connexion->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
