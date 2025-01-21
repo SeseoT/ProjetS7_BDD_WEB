1353563b525585858fc7bc6033b2ae2e906adbbc
SeseoT <tangy.suto@gmail.com>
2025-01-21 16:14:24 +0100

    Gestion dans admin de la requete 1

--- devTest/requete1.php
+++ devTest/requete1.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username'])) {
     exit();
 }else{//Redirection vers les pages spécialisé des clients
     try {
-        $sql = "SELECT DISTINCT prenom, nom, Utilisateur.adresse, Club.nomClub, Club.departement, Club.region, dateDebut, dateFin FROM Utilisateur, Club, Concours,Competiteur,CompetiteurParticipe WHERE Utilisateur.numUtilisateur = Competiteur.numCompetiteur AND Concours.numConcours = CompetiteurParticipe.numConcours AND CompetiteurParticipe.numCompetiteur = Competiteur.numCompetiteur AND Club.numClub = Utilisateur.numClub AND YEAR(Concours.dateFin) = '2023' ";
+        $sql = "SELECT Utilisateur.prenom, Utilisateur.nom, Utilisateur.adresse, Club.nomClub, Club.departement, Club.region, dateDebut, dateFin FROM Utilisateur, Club, Concours, Competiteur, CompetiteurParticipe WHERE Utilisateur.numUtilisateur = Competiteur.numCompetiteur AND Concours.numConcours = CompetiteurParticipe.numConcours AND CompetiteurParticipe.numCompetiteur = Competiteur.numCompetiteur AND Club.numClub = Utilisateur.numClub AND YEAR(Concours.dateFin) = "2023" GROUP BY Utilisateur.numUtilisateur, Utilisateur.prenom, Utilisateur.nom, Utilisateur.adresse, Club.nomClub, Club.departement, Club.region ";
         $stmt = $connexion->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -32,11 +32,11 @@ if (!isset($_SESSION['username'])) {
         $html .= '<tr>';
         $html .= '<td>' . htmlspecialchars($row['prenom']) . '</td>';
         $html .= '<td>' . htmlspecialchars($row['nom']) . '</td>';
-         $html .= '<td>' . htmlspecialchars($row['adresse']) . '</td>';
+        $html .= '<td>' . htmlspecialchars($row['adresse']) . '</td>';
         $html .= '<td>' . htmlspecialchars($row['nomClub']) . '</td>';
-         $html .= '<td>' . htmlspecialchars($row['departement']) . '</td>';
+        $html .= '<td>' . htmlspecialchars($row['departement']) . '</td>';
         $html .= '<td>' . htmlspecialchars($row['region']) . '</td>';
-         $html .= '<td>' . htmlspecialchars($row['dateDebut']) . '</td>';
+        $html .= '<td>' . htmlspecialchars($row['dateDebut']) . '</td>';
         $html .= '<td>' . htmlspecialchars($row['dateFin']) . '</td>';
         $html .= '</tr>';
     }
