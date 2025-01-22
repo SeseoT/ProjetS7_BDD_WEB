17b2f75042faa77712520715780482c9dde56fd9
SeseoT <tangy.suto@gmail.com>
2025-01-22 23:33:05 +0100

    Test Creation concours par un admin

--- devTest/insertConcours.php
+++ devTest/insertConcours.php
@@ -6,20 +6,20 @@ require("connect.php"); // Inclure la configuration de connexion
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
-}else{//Redirection vers les pages spécialisé des clients
+}else {//Redirection vers les pages spécialisé des clients
+    try {
+        $theme = htmlspecialchars(trim($_POST['theme']));
+        $date_debut = htmlspecialchars(trim($_POST['date_debut']));
+        $date_fin = htmlspecialchars(trim($_POST['date_fin']));
+        $description = htmlspecialchars(trim($_POST['description'])); // Récupération de la description
+        $numPresident = htmlspecialchars(trim($_POST['numPresident']));
 
-    $theme = htmlspecialchars(trim($_POST['theme']));
-    $date_debut = htmlspecialchars(trim($_POST['date_debut']));
-    $date_fin = htmlspecialchars(trim($_POST['date_fin']));
-    $description = htmlspecialchars(trim($_POST['description'])); // Récupération de la description
-    $numPresident = htmlspecialchars(trim($_POST['numPresident']));
-
-    if (strlen($description) > 200) {
-        echo "La description dépasse les 200 caractères autorisés.";
-        exit();
-    }
-    // Requête d'insertion
-    $query = "INSERT INTO Concours (theme, dateDebut, dateFin, etat, descriptif, numPresident) 
+        if (strlen($description) > 200) {
+            echo "La description dépasse les 200 caractères autorisés.";
+            exit();
+        }
+        // Requête d'insertion
+        $query = "INSERT INTO Concours (theme, dateDebut, dateFin, etat, descriptif, numPresident) 
 VALUES (
     :theme,
     :date_debut,
@@ -29,20 +29,23 @@ VALUES (
     :numPresident);";
 
 
-    $stmt = $connexion->prepare($query);
-    $stmt->bindParam(':theme', $theme);
-    $stmt->bindParam(':date_debut', $date_debut);
-    $stmt->bindParam(':date_fin', $date_fin);
-    $stmt->bindParam(':description', $description);
-    $stmt->bindParam(':numPresident', $numPresident);
+        $stmt = $connexion->prepare($query);
+        $stmt->bindParam(':theme', $theme);
+        $stmt->bindParam(':date_debut', $date_debut);
+        $stmt->bindParam(':date_fin', $date_fin);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':numPresident', $numPresident);
 
-    // Exécuter la requête
-    if ($stmt->execute()) {
-        header("Location: index.php"); // Rediriger vers le tableau de bord
-        exit();
-    } else {
-        header("Location: index.php"); // Rediriger vers le tableau de bord
-        exit();
+        // Exécuter la requête
+        if ($stmt->execute()) {
+            header("Location: index.php"); // Rediriger vers le tableau de bord
+            exit();
+        } else {
+            header("Location: index.php"); // Rediriger vers le tableau de bord
+            exit();
+        }
+    } catch (PDOException $e) {
+        die("Erreur lors de la connexion : " . $e->getMessage());
     }
 }
 ?>
